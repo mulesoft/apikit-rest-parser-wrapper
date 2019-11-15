abb76fc9ac70645ce70f53350cdffc93250bfc64
Juan Brasca <juan.brasca@gmail.com>
2019-11-15 16:14:05 -0300

    APIKIT-2055: Increase Parser Wrapper V1 implemetation coverage to 75%

---
+++ interface-impl-v1/src/test/java/org/mule/apikit/implv1/model/RamlImplV1Test.java
@@ -0,0 +1,85 @@
+package org.mule.apikit.implv1.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RamlImplV1Test {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getRaml() {
+    }
+
+    @Test
+    public void getResource() {
+    }
+
+    @Test
+    public void getConsolidatedSchemas() {
+    }
+
+    @Test
+    public void getCompiledSchemas() {
+    }
+
+    @Test
+    public void getBaseUri() {
+    }
+
+    @Test
+    public void getLocation() {
+    }
+
+    @Test
+    public void getResources() {
+    }
+
+    @Test
+    public void getVersion() {
+    }
+
+    @Test
+    public void getBaseUriParameters() {
+    }
+
+    @Test
+    public void getSecuritySchemes() {
+    }
+
+    @Test
+    public void getTraits() {
+    }
+
+    @Test
+    public void getUri() {
+    }
+
+    @Test
+    public void getSchemas() {
+    }
+
+    @Test
+    public void getInstance() {
+    }
+
+    @Test
+    public void getAllReferences() {
+    }
+
+    @Test
+    public void dump() {
+    }
+
+    @Test
+    public void getApiVendor() {
+    }
+
+    @Test
+    public void getType() {
+    }
+}
