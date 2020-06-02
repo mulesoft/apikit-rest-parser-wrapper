e717adf67b56531cfed224b5787dfcb03127805e
Nicolas Kosacoff <nicolas.kosacoff@mulesoft.com>
2020-06-02 15:03:09 -0300

    APIKIT-15479: Reorganise imports.

--- interface-impl-amf/src/test/java/org/mule/amf/impl/parser/rule/ApiValidationResultImplTest.java
+++ interface-impl-amf/src/test/java/org/mule/amf/impl/parser/rule/ApiValidationResultImplTest.java
@@ -22,8 +22,9 @@ import scala.Option;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
 
 public class ApiValidationResultImplTest {
 
