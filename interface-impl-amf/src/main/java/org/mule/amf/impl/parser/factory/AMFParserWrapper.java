c70d14d273556868fe5d6b25acadf0f91e377966
Damián Courtil <dcourtil@mulesoft.com>
2020-06-29 14:44:16 -0300

    APIKIT-2663: APIKit is preventing app to deploy on big API (#107)

--- interface-impl-amf/src/main/java/org/mule/amf/impl/parser/factory/AMFParserWrapper.java
+++ interface-impl-amf/src/main/java/org/mule/amf/impl/parser/factory/AMFParserWrapper.java
@@ -8,36 +8,57 @@ package org.mule.amf.impl.parser.factory;
 
 import amf.MessageStyle;
 import amf.ProfileName;
+import amf.client.AMF;
+import amf.client.environment.Environment;
+import amf.client.model.document.Document;
 import amf.client.parse.Parser;
 import amf.client.resolve.Resolver;
+import amf.client.validate.ValidationReport;
+import amf.plugins.document.webapi.resolution.pipelines.AmfResolutionPipeline;
+import org.mule.amf.impl.exceptions.ParserException;
+
+import java.net.URI;
+import java.net.URLDecoder;
+import java.util.concurrent.CompletableFuture;
 
 public class AMFParserWrapper {
 
+    private Environment environment;
     private Parser parser;
     private Resolver resolver;
     private ProfileName profileName;
     private MessageStyle messageStyle;
 
-    public AMFParserWrapper(Parser parser, Resolver resolver, ProfileName profileName, MessageStyle messageStyle) {
+    public AMFParserWrapper(Environment environment, Parser parser, Resolver resolver, ProfileName profileName, MessageStyle messageStyle) {
+        this.environment = environment;
         this.parser = parser;
         this.resolver = resolver;
         this.profileName = profileName;
         this.messageStyle = messageStyle;
     }
 
-    public Parser getParser() {
-        return parser;
+    public Document parseApi(final URI uri) throws ParserException {
+        final String url = URLDecoder.decode(uri.toString());
+        Document document = handleFuture(parser.parseFileAsync(url));
+        return (Document) resolver.resolve(document, AmfResolutionPipeline.EDITING_PIPELINE());
     }
 
-    public Resolver getResolver() {
-        return resolver;
+    public ValidationReport getParsingReport(Document resolvedDoc) throws ParserException {
+        return handleFuture(AMF.validateResolved(resolvedDoc, profileName, messageStyle, environment));
     }
 
-    public ProfileName getProfileName() {
-        return profileName;
+    private <T, U> U handleFuture(CompletableFuture<T> f) throws ParserException {
+        try {
+            return (U) f.get();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw getParseException(e);
+        } catch (Exception e) {
+            throw getParseException(e);
+        }
     }
 
-    public MessageStyle getMessageStyle() {
-        return messageStyle;
+    private static ParserException getParseException(Exception e) {
+        throw new ParserException("An error happened while parsing the api. Message: " + e.getMessage(), e);
     }
 }
