a1bb4abeb2b0747d2b7156072251ac7a86367602
JuanAller <juan.aller@mulesoft.com>
2020-08-13 11:56:24 -0300

    APIKIT-2692: Avoid AMF initialization when RAML parser is used (#131) (#132)

---
+++ parser-service/src/test/java/org/mule/parser/service/ReferencesResolverTestCase.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.parser.service;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.mule.apikit.model.api.ApiReference.create;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mule.apikit.model.api.ApiReference;
+import org.mule.parser.service.references.ReferencesResolver;
+import org.mule.parser.service.strategy.AMFParsingStrategy;
+
+public class ReferencesResolverTestCase {
+
+  @Test
+  public void amfThrowExceptionParsingApiReference() throws Exception {
+    AMFParsingStrategy parsingStrategy = Mockito.mock(AMFParsingStrategy.class);
+    when(parsingStrategy.parse(any(ApiReference.class))).thenThrow(new RuntimeException("Testing exception"));
+
+    setFinalStatic(ReferencesResolver.class.getDeclaredField("amfParsingStrategy"), parsingStrategy);
+
+    ReferencesResolver referencesResolver = new ReferencesResolver();
+
+    assertThat(referencesResolver.getReferences(create("path/to/api.raml")), equalTo(emptyList()));
+  }
+
+  private void setFinalStatic(Field field, Object newValue) throws Exception {
+    field.setAccessible(true);
+    Field modifiersField = Field.class.getDeclaredField("modifiers");
+    modifiersField.setAccessible(true);
+    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+    field.set(null, newValue);
+  }
+
+}
