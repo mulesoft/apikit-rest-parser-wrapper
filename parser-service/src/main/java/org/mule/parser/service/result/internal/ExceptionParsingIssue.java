d6997595290c099c30ddf08439c24c376ee2eec2
Damián Courtil <dcourtil@mulesoft.com>
2020-11-19 14:33:48 -0300

    APIKIT-2821: Move result implementations to internal package (#163) (#164)

--- parser-service/src/main/java/org/mule/parser/service/result/internal/ExceptionParsingIssue.java
+++ parser-service/src/main/java/org/mule/parser/service/result/internal/ExceptionParsingIssue.java
@@ -4,17 +4,21 @@
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
-package org.mule.parser.service.result;
+package org.mule.parser.service.result.internal;
+
+import org.mule.parser.service.result.ParsingIssue;
 
 /**
  * Represents a simple parsing error with cause message
  */
-public class DefaultParsingIssue implements ParsingIssue {
+public class ExceptionParsingIssue implements ParsingIssue {
 
   private final String cause;
+  private final String stackTrace;
 
-  public DefaultParsingIssue(String cause) {
+  public ExceptionParsingIssue(String cause, String stackTrace) {
     this.cause = cause;
+    this.stackTrace = stackTrace;
   }
 
   @Override
@@ -22,8 +26,15 @@ public class DefaultParsingIssue implements ParsingIssue {
     return cause;
   }
 
+  public String getStackTrace() {
+    return stackTrace;
+  }
+
   @Override
   public String toString() {
-    return cause;
+    return "ExceptionParsingIssue {" +
+        "cause='" + cause + '\'' +
+        ", stackTrace='" + stackTrace + '\'' +
+        '}';
   }
 }
