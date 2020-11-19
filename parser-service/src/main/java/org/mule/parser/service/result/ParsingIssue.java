34e77aae36f34a891eca42a2b0df70b3eaf23ebd
Damian Courtil <dcourtil@mulesoft.com>
2020-11-19 16:27:57 -0300

    APIKIT-2821: Expose code for identifying the error type

--- parser-service/src/main/java/org/mule/parser/service/result/ParsingIssue.java
+++ parser-service/src/main/java/org/mule/parser/service/result/ParsingIssue.java
@@ -17,4 +17,11 @@ public interface ParsingIssue {
    * @return a details cause of the error.
    */
   String cause();
+
+  /**
+   * Parsing issue code used for identifying the error type
+   *
+   * @return a details cause of the error.
+   */
+  ParsingIssueCode code();
 }
