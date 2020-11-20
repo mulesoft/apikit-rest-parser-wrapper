e5946b69dc0e70d0884597a81575d10ef5f10b4a
Damián Courtil <dcourtil@mulesoft.com>
2020-11-20 19:31:38 -0300

    APIKIT-2821: Move ParserService to internal package (#167) (#168)

--- parser-service/src/test/java/org/mule/parser/service/ParserServiceTestCase.java
+++ parser-service/src/test/java/org/mule/parser/service/ParserServiceTestCase.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mule.apikit.ApiType;
 import org.mule.apikit.model.api.ApiReference;
+import org.mule.parser.service.internal.ParserService;
 import org.mule.parser.service.result.ParsingIssue;
 import org.mule.parser.service.result.internal.ParseResult;
 
--- parser-service/src/test/java/org/mule/parser/service/oas/OasValidationReportTestCase.java
+++ parser-service/src/test/java/org/mule/parser/service/oas/OasValidationReportTestCase.java
@@ -9,7 +9,7 @@ package org.mule.parser.service.oas;
 import org.junit.Test;
 import org.mule.apikit.model.api.ApiReference;
 import org.mule.parser.service.ParserMode;
-import org.mule.parser.service.ParserService;
+import org.mule.parser.service.internal.ParserService;
 import org.mule.parser.service.ResourcesUtils;
 import org.mule.parser.service.result.internal.ParseResult;
 
