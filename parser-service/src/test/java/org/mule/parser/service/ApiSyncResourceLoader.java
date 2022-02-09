a5da115293b4c07bb2f54c8b75b9ef9fcfd50442
Juan Brasca <juan.brasca@gmail.com>
2022-02-09 10:32:33 -0300

    APIKIT-3151: fix APISync resource loader issue (#296)

---
+++ parser-service/src/test/java/org/mule/parser/service/ApiSyncResourceLoader.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.parser.service;
+
+import org.mule.apikit.loader.ResourceLoader;
+
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static java.lang.String.format;
+import static org.mule.apikit.common.ApiSyncUtils.isSyncProtocol;
+
+public class ApiSyncResourceLoader implements ResourceLoader {
+
+  private static final String protocol = "jar:file:";
+  private static final String API_SYNC_PROTOCOL = "resource::";
+  private final String localRepository =
+      new File("").getAbsolutePath().replace("\\", "/") + "/src/test/resources/.m2/repository";
+
+  @Override
+  public URI getResource(String resource) {
+    try {
+      if (!isSyncProtocol(resource)) {
+        return null;
+      }
+
+      String[] parts = resource.substring(API_SYNC_PROTOCOL.length()).split(":");
+
+      String groupId = parts[0].replaceAll("\\.", "/");
+      String artifact = parts[1];
+      String version = parts[2];
+      String classifier = parts[3];
+      String file = parts[5];
+
+      return new URI(
+                     format("%s%s/%s/%s/%s/%s-%s-%s.zip!/%s", protocol, localRepository,
+                            groupId, artifact, version, artifact, version, classifier, file));
+    } catch (URISyntaxException e) {
+      return null;
+    }
+  }
+
+}
