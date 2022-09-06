b5a3dda701aad2563bbdfb106c18baaf64934966
Camila Rocca <crocca@mulesoft.com>
2022-09-06 12:09:29 -0300

    W-11687519: Fix ApiSync resource loader issue with filenames containing spaces
    Signed-off-by: Camila Rocca <crocca@mulesoft.com>

--- parser-service/src/test/java/org/mule/parser/service/ApiSyncTestCase.java
+++ parser-service/src/test/java/org/mule/parser/service/ApiSyncTestCase.java
@@ -20,6 +20,12 @@ public class ApiSyncTestCase {
   }
 
   @Test
+  public void resourceNameWithSpacesTestCase() {
+    assertSuccessfulParsing("resource::c8cdb7d6-e052-449a-97f3-049f032ad03a:american-flights-api:1.0.5:raml:zip:american flights api.raml");
+  }
+
+
+  @Test
   public void oasApiToRamlFragmentTestCase() {
     assertSuccessfulParsing("resource::820a4865-0c0a-4885-b029-25d6f5b31c9b:oas-api-test:1.0.0:oas:zip:oas-api-test.json");
   }
