6738f466f239133c7700b088103c7e98f58d7670
JuanAller <juanmariaaller@gmail.com>
2023-05-24 18:25:13 -0300

    W-13273173 : include URI in AMF Content when fetching a resource (#449) (#450)

--- parser-service/src/test/java/org/mule/parser/service/ApiSyncTestCase.java
+++ parser-service/src/test/java/org/mule/parser/service/ApiSyncTestCase.java
@@ -6,8 +6,13 @@
  */
 package org.mule.parser.service;
 
+import java.util.List;
+import java.util.Map;
 import org.junit.Test;
+import org.mule.apikit.model.ApiSpecification;
+import org.mule.apikit.model.Resource;
 import org.mule.apikit.model.api.ApiReference;
+import org.mule.apikit.validation.ApiValidationResult;
 import org.mule.parser.service.result.ParseResult;
 
 import static org.junit.Assert.assertTrue;
@@ -16,23 +21,42 @@ public class ApiSyncTestCase {
 
   @Test
   public void fundamentalsTrainingTestCase() {
-    assertSuccessfulParsing("resource::68ef9520-24e9-4cf2-b2f5-620025690913:training-american-flights-api:2.0.1:raml:zip:american-flights-api.raml");
+    assertTrue(getResult("resource::68ef9520-24e9-4cf2-b2f5-620025690913:training-american-flights-api:2.0.1:raml:zip:american-flights-api.raml")
+        .success());
   }
 
   @Test
   public void resourceNameWithSpacesTestCase() {
-    assertSuccessfulParsing("resource::c8cdb7d6-e052-449a-97f3-049f032ad03a:american-flights-api:1.0.5:raml:zip:american flights api.raml");
+    assertTrue(getResult("resource::c8cdb7d6-e052-449a-97f3-049f032ad03a:american-flights-api:1.0.5:raml:zip:american flights api.raml")
+        .success());
   }
 
 
   @Test
   public void oasApiToRamlFragmentTestCase() {
-    assertSuccessfulParsing("resource::820a4865-0c0a-4885-b029-25d6f5b31c9b:oas-api-test:1.0.0:oas:zip:oas-api-test.json");
+    assertTrue(getResult("resource::820a4865-0c0a-4885-b029-25d6f5b31c9b:oas-api-test:1.0.0:oas:zip:oas-api-test.json")
+        .success());
   }
 
-  public void assertSuccessfulParsing(String location) {
+  @Test
+  public void parseXsdWithInclude() {
+    ParseResult parserResult =
+        getResult("resource::820a4865-0c0a-4885-b029-25d6f5b31c9b:xsd-with-include:1.0.0:raml:zip:api.raml");
+
+    ApiSpecification specification = parserResult.get();
+
+    Map<String, Resource> resources = specification.getResources();
+    List<ApiValidationResult> results = resources
+        .get("/test")
+        .getAction("PUT")
+        .getBody()
+        .get("application/xml")
+        .validate("<message xmlns=\"http://www.example.org/simple\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.example.org/simple schema.xsd\" item=\"content\"/>");
+    assertTrue(results.isEmpty());
+  }
+
+  private ParseResult getResult(String location) {
     ApiReference apiReference = ApiReference.create(location, new ApiSyncResourceLoader());
-    ParseResult parserResult = new ParserService().parse(apiReference);
-    assertTrue(parserResult.success());
+    return new ParserService().parse(apiReference);
   }
 }
