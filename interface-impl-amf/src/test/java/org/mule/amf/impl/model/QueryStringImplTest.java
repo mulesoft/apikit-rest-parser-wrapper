77613e5deccf1d87ef74c9afc86a0412ba63cefa
Juan Brasca <juan.brasca@gmail.com>
2023-09-11 12:12:12 -0300

    W-14088058: amf versionn -> 5.4.1-6

--- interface-impl-amf/src/test/java/org/mule/amf/impl/model/QueryStringImplTest.java
+++ interface-impl-amf/src/test/java/org/mule/amf/impl/model/QueryStringImplTest.java
@@ -17,6 +17,7 @@ import java.util.Collection;
 import java.util.HashMap;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
@@ -28,12 +29,13 @@ public class QueryStringImplTest {
   private QueryString locationsQueryString;
   private QueryString emailQueryString;
   private QueryString historySinceQueryString;
+  private QueryString petsQueryString;
 
   @Before
   public void setUp() throws Exception {
     String apiLocation = this.getClass().getResource("../10-query-string/api.raml").toURI().toString();
     ApiReference apiRef = ApiReference.create(apiLocation);
-    ApiSpecification api = new AMFParser(apiRef, true).parse();
+    ApiSpecification api = new AMFParser(apiRef).parse();
     ResourceImpl resource = (ResourceImpl) api.getResource("/locations");
     ActionImpl action = (ActionImpl) resource.getAction(GET_ACTION);
     locationsQueryString = action.queryString();
@@ -42,6 +44,8 @@ public class QueryStringImplTest {
     emailQueryString = action.queryString();
     action = (ActionImpl) emails.getResources().get("/historySince").getAction(GET_ACTION);
     historySinceQueryString = action.queryString();
+    action = (ActionImpl) emails.getResources().get("/pets").getAction(GET_ACTION);
+    petsQueryString = action.queryString();
   }
 
   @Test
@@ -59,26 +63,63 @@ public class QueryStringImplTest {
     assertTrue(locationsQueryString.validate(new HashMap<String, Collection<?>>() {
 
       {
-        put("start", asList("2"));
-        put("lat", asList("12"));
-        put("long", asList("13"));
+        put("start", singletonList("2"));
+        put("lat", singletonList("12"));
+        put("long", singletonList("13"));
       }
     }));
+
+    assertTrue(locationsQueryString.validate(new HashMap<String, Collection<?>>() {
+
+      {
+        put("start", singletonList("2"));
+        put("page-size", singletonList("20"));
+        put("location", singletonList("1,2"));
+      }
+    }));
+
     assertFalse(locationsQueryString.validate(new HashMap<String, Collection<?>>() {
 
       {
-        put("Not valid query string", asList("bad"));
+        put("Not valid query string", singletonList("bad"));
       }
     }));
-    assertTrue(emailQueryString
-        .validate(new HashMap<String, Collection<?>>() {
 
-          {
-            put("subject", asList("Email Subject"));
-            put("to", asList("John", "Ana"));
-            put("body", asList("Email body"));
-          }
-        }));
+    assertTrue(emailQueryString.validate(new HashMap<String, Collection<?>>() {
+
+      {
+        put("subject", singletonList("Email Subject"));
+        put("to", asList("John", "Ana"));
+        put("body", singletonList("Email body"));
+      }
+    }));
+  }
+
+  @Test
+  public void multipleUnionsValidationTest() {
+    assertTrue(petsQueryString.validate(new HashMap<String, Collection<?>>() {
+
+      {
+        put("wings", singletonList("2"));
+        put("name", singletonList("santa"));
+      }
+    }));
+
+    assertTrue(petsQueryString.validate(new HashMap<String, Collection<?>>() {
+
+      {
+        put("color", singletonList("red"));
+        put("name", singletonList("santa"));
+      }
+    }));
+
+    assertFalse(petsQueryString.validate(new HashMap<String, Collection<?>>() {
+
+      {
+        put("color", singletonList("blue"));
+        put("wings", singletonList("1"));
+      }
+    }));
   }
 
   @Test
